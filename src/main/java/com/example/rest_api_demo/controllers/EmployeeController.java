2a4491c255ad9cca65a750e581fcb9b58435dc0e
Jenish Detroja <jenish.detroja@dialogos.fi>
2026-02-03 08:26:42 +0530

    caching implemetation

--- src/main/java/com/example/rest_api_demo/controllers/EmployeeController.java
+++ src/main/java/com/example/rest_api_demo/controllers/EmployeeController.java
@@ -7,14 +7,24 @@ import com.example.rest_api_demo.dtos.requests.UpdateEmployeeDepartmentRequestDT
 import com.example.rest_api_demo.entities.Employee;
 import com.example.rest_api_demo.services.EmployeeService;
 import com.example.rest_api_demo.utils.FileHandler;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.http.CacheControl;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.context.request.ServletWebRequest;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
+import java.net.MalformedURLException;
 import java.util.List;
-
+import java.util.concurrent.TimeUnit;
 
 
 @RestController
@@ -27,6 +37,14 @@ public class EmployeeController {
         this.employeeService = employeeService;
     }
 
+    @Operation(
+            description = "create employee (routing level versoning v1)",
+            responses = {
+                    @ApiResponse(responseCode ="201",description = "employee created"),
+                    @ApiResponse(responseCode ="400",description = "validation failed"),
+                    @ApiResponse(responseCode ="500",description = "internal server error")
+            }
+    )
     @PostMapping(path = "/v1/employees")
     public Employee InsertEmployee(  @RequestPart  InsertEmployeeRequestDTO employee, @RequestPart MultipartFile profileImgFile){
         System.out.println("uri versoning:- v1 called ");
@@ -36,6 +54,14 @@ public class EmployeeController {
     }
 
     @PostMapping(path = "/v2/employees")
+    @Operation(
+            description = "create employee (routing level versoning v2)",
+            responses = {
+                    @ApiResponse(responseCode ="201",description = "employee created"),
+                    @ApiResponse(responseCode ="400",description = "validation failed"),
+                    @ApiResponse(responseCode ="500",description = "internal server error")
+            }
+    )
     public Employee InsertEmployeeV2(@RequestPart InsertEmployeeRequestDTO employee, @RequestPart MultipartFile profileImgFile){
         System.out.println("uri versoning:- v2 called ");
         String filePath = FileHandler.Save(profileImgFile,"profileImgs");
@@ -44,24 +70,56 @@ public class EmployeeController {
     }
 
     @PutMapping(value = "/employees/{employeeId}" ,params = "version=1")
+    @Operation(
+            description = "update employee (param level versoning v1)",
+            responses = {
+                    @ApiResponse(responseCode ="200",description = "employee created"),
+                    @ApiResponse(responseCode ="400",description = "validation failed"),
+                    @ApiResponse(responseCode ="500",description = "internal server error")
+            }
+    )
     public Employee updateEmployee(@PathVariable Long employeeId,   @Validated(OnCreate.class) @RequestBody InsertEmployeeRequestDTO employee){
         System.out.println("param versoning : v1 called ");
         return employeeService.updateEmployee(employeeId,employee);
     }
 
     @PutMapping(value = "/employees/{employeeId}" ,params = "version=2")
+    @Operation(
+            description = "update employee (param level versoning v2)",
+            responses = {
+                    @ApiResponse(responseCode ="200",description = "employee created"),
+                    @ApiResponse(responseCode ="400",description = "validation failed"),
+                    @ApiResponse(responseCode ="500",description = "internal server error")
+            }
+    )
     public Employee updateEmployeeV2(@PathVariable Long employeeId, @RequestBody InsertEmployeeRequestDTO employee){
         System.out.println("param versoning : v2 called ");
         return employeeService.updateEmployee(employeeId,employee);
     }
 
     @PatchMapping(value = "/employees/{employeeId}/update-department", headers = "X-API-VERSION=1")
+    @Operation(
+            description = "update employee department (header level versoning v1)",
+            responses = {
+                    @ApiResponse(responseCode ="200",description = "employee created"),
+                    @ApiResponse(responseCode ="400",description = "validation failed"),
+                    @ApiResponse(responseCode ="500",description = "internal server error")
+            }
+    )
     public Employee updateDepartmentOnly(@PathVariable Long employeeId, @RequestBody UpdateEmployeeDepartmentRequestDTO reqDTO){
         System.out.println("HEADER versoning : v1 called ");
         return employeeService.updateEmployeeDepartment(employeeId,reqDTO);
     }
 
     @PatchMapping("/v1/employees/{employeeId}/profile-img")
+    @Operation(
+            description = "update employee department (header level versoning v2)",
+            responses = {
+                    @ApiResponse(responseCode ="200",description = "employee created"),
+                    @ApiResponse(responseCode ="400",description = "validation failed"),
+                    @ApiResponse(responseCode ="500",description = "internal server error")
+            }
+    )
     public Employee changeProfileImg(@PathVariable Long employeeId, @RequestPart MultipartFile profileImgFile){
         String imgPath = FileHandler.Save(profileImgFile,"profileImgs");
         Employee employee = employeeService.updateProfileImg(employeeId,imgPath);
@@ -69,13 +127,39 @@ public class EmployeeController {
     }
 
     @GetMapping("/v1/employees")
+    @Operation(
+            description = "get all employees",
+            responses = {
+                    @ApiResponse(responseCode ="200",description = "employee list returned"),
+                    @ApiResponse(responseCode ="500",description = "internal server error")
+            }
+    )
     public List<Employee>  getAll(){
         return employeeService.getAllEmployees();
     }
 
     @GetMapping("/v1/employees/{employeeId}")
-    public Employee  getAll(@PathVariable Long employeeId){
-        return employeeService.getEmployeeById(employeeId);
+    public ResponseEntity<Employee> getEmployeeId(@PathVariable Long employeeId){
+//        ResponseEntity.
+        return ResponseEntity.ok()
+
+                .cacheControl(CacheControl.maxAge(1, TimeUnit.MINUTES).cachePublic())
+                .body(employeeService.getEmployeeById(employeeId));
+    }
+
+    @GetMapping("/v1/employees/profile-img/{imgName}")
+    public ResponseEntity<Resource> getProfileImg(@PathVariable String imgName, ServletWebRequest request) throws IOException {
+        Resource file = FileHandler.Get("profileImgs",imgName);
+        long lastModified = file.lastModified();
+        if(request.checkNotModified(lastModified)){
+            return ResponseEntity.status(HttpStatus.NOT_MODIFIED).build();
+        }
+        return ResponseEntity.ok()
+                .contentType(MediaType.IMAGE_PNG)
+                .cacheControl(CacheControl.maxAge(5,TimeUnit.MINUTES).cachePublic())
+                .lastModified(lastModified)
+                .body(file);
+
     }
 
 }
--- src/main/java/com/example/rest_api_demo/utils/FileHandler.java
+++ src/main/java/com/example/rest_api_demo/utils/FileHandler.java
@@ -1,14 +1,21 @@
 package com.example.rest_api_demo.utils;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
 import java.io.IOException;
+import java.net.MalformedURLException;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class FileHandler {
+    @Value("rest-api-demo.uploadFolder")
+    private static String uploadFolder;
     public static String Save(MultipartFile file, String folderName){
-        String filePath = System.getProperty("user.dir") + File.separator + "Uploads" + File.separator + folderName + File.separator + file.getOriginalFilename();
+        String filePath = System.getProperty("user.dir") + File.separator + "uploads"+ File.separator + folderName + File.separator + file.getOriginalFilename();
         File file1 = new File(filePath);
         try {
             file.transferTo(file1);
@@ -16,5 +23,13 @@ public class FileHandler {
             throw new RuntimeException(e);
         }
         return folderName + File.separator + file.getOriginalFilename();
+    }
+
+    public static Resource Get(String folderName,String fileName) throws MalformedURLException {
+        String absFilePath = System.getProperty("user.dir") + File.separator + "uploads" + File.separator + folderName + File.separator + fileName;
+        Path filePath = Paths.get( absFilePath);
+        Resource file = new UrlResource(filePath.toUri());
+
+        return file;
     }
 }
